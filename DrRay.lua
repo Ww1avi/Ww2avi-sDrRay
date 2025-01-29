a9c63682c40c2c9c77ede943e699f06886eb6136
Ww1avi <kandonmezmomo@gmail.com>
2025-01-29 19:44:35 +0100

    DrRay.lua

--- DrRay.lua
+++ DrRay.lua
@@ -1,6 +1,6 @@
 local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
 
-local window = DrRayLibrary:Load("DrRay", "Default")
+local window = DrRayLibrary:Load("Ww2avi's Script", "Default")
 
 local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
 
