a059684cb1d908d590ea3006bcd51657e783b00a
Ww1avi <kandonmezmomo@gmail.com>
2025-01-30 17:42:07 +0100

    DrRay.lua

--- DrRay.lua
+++ DrRay.lua
@@ -4,6 +4,6 @@ local window = DrRayLibrary:Load("Ww2avi's Script", "Default")
 
 local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
 
-tab.newButton("Auto Lift Weight", "Starts auto Lift Weight!", game:GetService("Players").LocalPlayer.muscleEvent:FireServer("rep")
-    print('Hello!')
+tab.newButton("Auto Lifts weight", "Starts Auto Lift weight!", function()
+    game:GetService("Players").LocalPlayer.muscleEvent:FireServer("rep")
 end)
